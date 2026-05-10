96cc9080b149b24aa29e9ff995b554e5a5ea4c67
Andrea Marson <am.dev.75@gmail.com>
2026-05-10 13:16:45 +0200

    Version 0.1.1.

--- src/retriva_gateway/config.py
+++ src/retriva_gateway/config.py
@@ -15,7 +15,7 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from typing import List
 
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 
 class Settings(BaseSettings):
     GATEWAY_HOST: str = "0.0.0.0"
